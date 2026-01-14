071e2adde9a6229d0ee824cde4a20675d408ea9e
Basel-Abd-Alsous <basel.alsous2000@gmail.com>
2026-01-14 14:43:40 +0300

    Completed Changes

--- android/app/src/main/kotlin/com/example/jawad_captain/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/jawad_captain/MainActivity.kt
@@ -1,5 +1,29 @@
 package com.primetag.jawad_driver
 
 import io.flutter.embedding.android.FlutterActivity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import android.os.Bundle
+import android.content.Context
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "foreground_channel",
+                "Location Service",
+                NotificationManager.IMPORTANCE_LOW
+            )
+            val manager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
+    }
+}
